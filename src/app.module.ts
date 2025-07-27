d9561ba3d925343e93d9f0687c7fd476c4fc577b
alex <alexander-eu@hotmail.com>
2025-07-27 21:17:37 +0300

    Registration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { RegisterModule } from '@/users/register.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     AuthModule,
+    RegisterModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '7d' },
