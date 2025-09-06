4bf3a542e1cfd81f8fef48a680877fc94251b7fe
alex <alexander-eu@hotmail.com>
2025-09-06 18:30:26 +0300

    Refactor import path for PrismaModule and clean up files.

--- src/dungeons/dto/dungeon-details-query.dto.ts
+++ src/dungeons/dto/dungeon-details-query.dto.ts
@@ -15,3 +15,4 @@ export class DungeonDetailsQueryDto {
   itemPower?: number;
 }
 
+
--- src/dungeons/dungeons.module.ts
+++ src/dungeons/dungeons.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DungeonsController } from './dungeons.controller';
 import { DungeonsService } from './dungeons.service';
-import { PrismaModule } from '../prisma/prisma.module';
+import { PrismaModule } from '@/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
